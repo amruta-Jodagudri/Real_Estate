f6040055b314786af83ca8791c2c661ef25867a4
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-04-14 13:04:04 +0530

    Completed Sign up page

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
     const [formData, setformData] = useState({});
+    const [Error, setError] = useState(null);
+    const [loading, setloading] = useState(false);
+    const navigate = useNavigate();
+
     const handleChange = (e) => {
         setformData({
             ...formData,
@@ -11,17 +15,30 @@ export default function SignUp() {
     };
     const handleSubmit =async (e)=>{
         e.preventDefault();
-        const res = await fetch('/api/auth/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            console.log(data);
+        try {
+            setloading(true)
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+                });
+                const data = await res.json();
+                if(data.success === false){
+                    setError(data.message)
+                    setloading(false)
+                    return;
+                }
+                setloading(false)
+                setError(null)
+                navigate('/signIn')
+                console.log(data);
+        } catch (err) {
+            setloading(false)
+            setError(err.message)
+        }
     }
-    console.log(formData)
 
     return (
         <div className='p-3 max-w-lg mx-auto'>
@@ -30,7 +47,7 @@ export default function SignUp() {
             <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
             <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
             <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-            <button className='text-white bg-slate-700 p-3 rounded-lg uppercase hover:opacity-80'>Sign Up</button>
+            <button disabled={loading} className='text-white bg-slate-700 p-3 rounded-lg uppercase hover:opacity-80'>{loading?'Loading...':'Sign Up'}</button>
         </form>
         <div className="flex gap-2 mt-5">
             <p>Have an account?</p>
@@ -38,6 +55,7 @@ export default function SignUp() {
                 <span className='text-blue-700'>Sign In</span>
             </Link>
         </div>
+        {Error&&<p className='text-red-500 mt-5'>{Error}</p>}
         </div>
     )
 }
