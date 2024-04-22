0cc60a0a8077a428041d215b8316b5b5a09b633f
Amruta Jodagudri <144842080+amruta-Jodagudri@users.noreply.github.com>
2024-04-22 22:49:07 +0530

    Updated header and make profile page private

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Header from './Components/Header';
+import PrivateRoute from './Components/PrivateRoute';
 import About from './Pages/About';
 import Home from './Pages/Home';
 import Profile from './Pages/Profile';
@@ -15,9 +16,11 @@ export default function App() {
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
-            <Route path='/profile' element={<Profile/>}/>
             <Route path='/signIn' element={<SignIn/>}/>
             <Route path='/signup' element={<SignUp/>}/>
+            <Route element={<PrivateRoute/>}>
+              <Route path='/profile' element={<Profile/>}/>
+            </Route>
         </Routes>
       </Router>
     </div>
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+
+    const {currentUser} = useSelector(state=>state.user)
     return (
         <header className='bg-slate-200 shadow-md'>
             <div className="flex justify-between item-center max-w-6xl mx-auto p-3">
@@ -14,10 +17,29 @@ export default function Header() {
                     <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
                     <FaSearch className='text-slate-600'/>
                 </form>
-                <ul className='flex gap-4 mt-3 font-semibold'>
-                    <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
-                    <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
-                    <Link to='/signIn'><li className='text-slate-700 hover:underline'>{''}Sign In</li></Link>
+                <ul className='flex gap-4 pt-4 text-center'>
+                    <Link to='/'>
+                        <li className='hidden text-xl sm:inline font-bold text-slate-700 hover:underline'>
+                        Home
+                        </li>
+                    </Link>
+                    <Link to='/about'>
+                        <li className='hidden text-xl sm:inline font-bold text-slate-700 hover:underline'>
+                        About
+                        </li>
+                    </Link>
+                    <Link to='/signIn'>
+                        {currentUser ? (
+                        <Link to='/profile'>
+                            <img
+                            className='rounded-full h-10 w-10 object-cover'
+                            src={currentUser.avatar}
+                            alt='profile'/>
+                        </Link>
+                        ) : (
+                        <li className=' text-slate-700 text-xl font-bold hover:underline'> Sign in</li>
+                        )}
+                    </Link>
                 </ul>
             </div>
         </header>
@@ -26,7 +48,7 @@ export default function Header() {
 
 
 
-//        poor Stylling----------------
+//--------------poor Stylling----------------
 // import React, { useState } from 'react';
 // import { FaBars, FaSearch, FaTimes } from 'react-icons/fa';
 // import { Link } from 'react-router-dom';
